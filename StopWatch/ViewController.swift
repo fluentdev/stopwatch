6185535c1b61238e96a1714f61ed5bf2d2c58d68
Daina Corey <daina.corey@gmail.com>
2014-11-13 23:56:42 -0500

    Initial upload

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -7,19 +7,77 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    
+    var count = 0
+    var hours = 0
+    var mins = 0
+    var seconds = 0
+    var timer = NSTimer()
+    
+    @IBOutlet weak var lblTimer: UILabel!
+    
+    @IBAction func startStopWatch(sender: AnyObject) {
+        if(!timer.valid) {
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: Selector("result"), userInfo: nil, repeats: true)
+            println("Timer Started")
+        } else {
+            println("Timer already started!")
+        }
+    }
+    
+    @IBAction func pauseStopWatch(sender: AnyObject) {
+        timer.invalidate()
+        println("Timer Paused")
+    }
+    
+    @IBAction func cancelStopWatch(sender: AnyObject) {
+        timer.invalidate()
+        count = 0
+        hours = 0
+        mins = 0
+        seconds = 0
+        lblTimer.text = "00:00:00.00"
+        println("Timer Cancelled")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        lblTimer.text = "00:00:00.00"
     }
-
+    
+    func result() {
+        count++
+        if(count==100) {
+            count = 0
+            seconds++
+        }
+        
+        if(seconds == 60) {
+            seconds = 0
+            mins++
+        }
+        
+        if(mins == 60) {
+            mins = 0
+            hours++
+        }
+        //        NSString *paddedStr = [NSString stringWithFormat:@"%010d", 42];
+        var hh = String(format: "%02d",hours)
+        var mm = String(format: "%02d", mins)
+        var ss = String(format: "%02d", seconds)
+        var cc = String(format: "%02d", count)
+        
+        lblTimer.text = "\(hh):\(mm):\(ss).\(cc)"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
